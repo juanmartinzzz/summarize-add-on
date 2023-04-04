3a55c1110cbbbc61b4f90cbef1774be8a826f8d9
Juan <juan@Juans-MacBook-Air-personal.local>
2023-04-03 21:39:59 -0400

    First commit

---
+++ src/globals.js
@@ -0,0 +1,44 @@
+const commandKeys = {
+    regularSmall: 'regularSmall',
+    regularMedium: 'regularMedium',
+    regularLarge: 'regularLarge',
+    bulletsSmall: 'bulletsSmall',
+    bulletsMedium: 'bulletsMedium',
+    bulletsLarge: 'bulletsLarge',
+    simpleWordsSmall: 'simpleWordsSmall',
+    simpleWordsMedium: 'simpleWordsMedium',
+    simpleWordsLarge: 'simpleWordsLarge',
+    mindMapSmall: 'mindMapSmall',
+    mindMapMedium: 'mindMapMedium',
+    mindMapLarge: 'mindMapLarge',
+}
+
+const optionHintMap = {
+    [commandKeys.regularSmall]: 'Summarize (short)',
+    [commandKeys.regularMedium]: 'Summarize (medium)',
+    [commandKeys.regularLarge]: 'Summarize (long)',
+    [commandKeys.bulletsSmall]: 'Summarize using bullet points (short)',
+    [commandKeys.bulletsMedium]: 'Summarize using bullet points (medium)',
+    [commandKeys.bulletsLarge]: 'Summarize using bullet points (long)',
+    [commandKeys.simpleWordsSmall]: 'Summarize using simple language (short)',
+    [commandKeys.simpleWordsMedium]: 'Summarize using simple language (medium)',
+    [commandKeys.simpleWordsLarge]: 'Summarize using simple language (long)',
+    [commandKeys.mindMapSmall]: 'Summarize using multi-nested bullet points (short)',
+    [commandKeys.mindMapMedium]: 'Summarize using multi-nested bullet points (medium)',
+    [commandKeys.mindMapLarge]: 'Summarize using multi-nested bullet points (long)',
+}
+
+const commandMap = {
+    [commandKeys.regularSmall]: '------------- summarize the text in 25 words or less',
+    [commandKeys.regularMedium]: '------------- summarize the text in 50 words or less',
+    [commandKeys.regularLarge]: '------------- summarize the text',
+    [commandKeys.bulletsSmall]: '------------- summarize the text using bullet points, in 50 words or less',
+    [commandKeys.bulletsMedium]: '------------- summarize the text using bullet points, in 90 words or less',
+    [commandKeys.bulletsLarge]: '------------- summarize the text using bullet points',
+    [commandKeys.simpleWordsSmall]: '------------- explain this text to me like I was a 10 year old child, in 25 words or less',
+    [commandKeys.simpleWordsMedium]: '------------- explain this text to me like I was a 10 year old child, in 50 words or less',
+    [commandKeys.simpleWordsLarge]: '------------- explain this text to me like I was a 10 year old child',
+    [commandKeys.mindMapSmall]: '------------- summarize the text using a text-based mind map using nested bullet points, in 50 words or less',
+    [commandKeys.mindMapMedium]: '------------- summarize the text using a text-based mind map using nested bullet points, in 90 words or less',
+    [commandKeys.mindMapLarge]: '------------- summarize the text using a text-based mind map using nested bullet points',
+}
---
+++ src/ui-elements.js
@@ -0,0 +1,83 @@
+const menuOptionTypes = {
+    regular: [commandKeys.regularSmall, commandKeys.regularMedium, commandKeys.regularLarge],
+    bullets: [commandKeys.bulletsSmall, commandKeys.bulletsMedium, commandKeys.bulletsLarge],
+    simpleWords: [commandKeys.simpleWordsSmall, commandKeys.simpleWordsMedium, commandKeys.simpleWordsLarge],
+    mindMap: [commandKeys.mindMapSmall, commandKeys.mindMapMedium, commandKeys.mindMapLarge],
+};
+
+const removeElementById = ({ id }) => {
+    const element = document.getElementById(id);
+    if (element) {
+        element.remove();
+    }
+}
+
+const getMenuOption = ({}) => {
+    const MenuOption = 1;
+
+    return MenuOption;
+}
+
+const getCloseWindowIcon = () => {
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    
+    svg.classList.add('iconButton');
+    svg.setAttribute("width", "16");
+    svg.setAttribute("height", "16");
+    svg.setAttribute("viewBox", "0 0 16 16");
+    path.setAttribute("fill", "currentColor");
+    path.setAttribute("d", "M15 1.5l-1.5-1.5-6.5 6.5-6.5-6.5-1.5 1.5 6.5 6.5-6.5 6.5 1.5 1.5 6.5-6.5 6.5 6.5 1.5-1.5-6.5-6.5z");
+
+    svg.appendChild(path);
+    
+    return svg;
+}
+
+const showSummary = ({ completion }) => {
+    const card = createElementWithAttributes({type: 'div', attributes: {id: summaryCardElementId, class: 'card'}});
+    const header = createElementWithAttributes({type: 'div', attributes: {class: 'header'}});
+    const title  = createElementWithAttributes({type: 'div', attributes: {innerText: 'Results by SummarizeGPT'}});
+    const closeWindow = getCloseWindowIcon();
+    const body = createElementWithAttributes({type: 'div', attributes: {class: 'body'}});
+
+    closeWindow.addEventListener('click', () => removeElementById({ id: summaryCardElementId }));
+
+    if (completion.error) {
+        body.innerText = `${completion.error.code}:\n\r${completion.error.message}`;
+    } else {
+        body.innerText = completion.choices[0].message.content;
+    }
+
+    header.appendChild(title);
+    header.appendChild(closeWindow);
+    card.appendChild(header);
+    card.appendChild(body);
+    Array.from(document.getElementsByTagName('body'))[0].appendChild(card);
+}
+
+const addActionMenu = ({ element }) => {
+    const actionMenu = createElementWithAttributes({type: 'div', attributes: {id: actionMenuElementId, class: 'actionMenu'}});
+
+    // Get the selected text as a string
+    const selection = window.getSelection();
+    const selectedText = selection.toString();
+
+    Object.keys(menuOptionTypes).map(type => {
+        const menuOptionGroup = createElementWithAttributes({type: 'div', attributes: {class: 'menuOptionGroup'}});
+
+        menuOptionTypes[type].map(commandKey => {
+            const menuOption = createElementWithAttributes({type: 'div', attributes: {class: `menuOption ${commandKey}`}});
+            const menuOptionHint = createElementWithAttributes({type: 'div', attributes: {class: `menuOptionHint`, innerText: optionHintMap[commandKey]}});
+            menuOption.addEventListener('click', () => summarize({ selectedText, commandKey }));
+            
+            menuOption.appendChild(menuOptionHint);
+            menuOptionGroup.appendChild(menuOption);
+        })
+
+        actionMenu.appendChild(menuOptionGroup);
+    })
+
+    // element.parentElement.appendChild(actionMenu);
+    Array.from(document.getElementsByTagName('body'))[0].appendChild(actionMenu);
+}
